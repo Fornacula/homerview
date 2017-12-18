ecb0c764dddb5aa6f749c69f79c03fbcc21bcba7
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2017-12-18 15:56:10 +0200

    Add invoice_nr to invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215170102) do
+ActiveRecord::Schema.define(version: 20171218135443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171215170102) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invoice_nr"
     t.index ["service_id"], name: "index_invoices_on_service_id"
     t.index ["user_id"], name: "index_invoices_on_user_id"
   end
