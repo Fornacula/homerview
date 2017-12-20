a4d80c9b73e9008066b435a4520d47e0036f46b6
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2017-12-20 18:51:54 +0200

    Service create show and destroy actions

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,39 @@
 class ServicesController < ApplicationController
+
+  before_action :set_service, only: [:show, :destroy]
+
+  def new; end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to services_path, notice: t('services.successful_save')
+    else
+      redirect_to new_service_path, alert: @service.errors.full_messages.join(', ')
+    end
+  end
+
+  def show; end
+
+  def destroy
+    if @service.destroy
+      redirect_to services_path, notice: t('services.successful_destroy')
+    else
+      redirect_to services_path, alert: @service.errors.full_messages.join(', ')
+    end
+  end
+
   def index
     @services = Service.all
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:name)
+  end
+
+  def set_service
+    @service = Service.find(params[:id])
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,4 @@
 class Service < ApplicationRecord
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
   validates :name, uniqueness: true
 end
