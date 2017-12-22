9ebff33fd195122f4b86cc065bcbe27691f94004
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2017-12-22 13:32:03 +0200

    Root to invoices #index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|et/ do
     devise_for :users
-    root to: 'users#index'
+    root to: 'invoices#index'
     resources :services
     resources :invoices
   end
