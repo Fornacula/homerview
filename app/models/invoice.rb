f5cc2609f85654dafe2c89e42e6cf015c0ab1944
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2017-12-26 16:48:58 +0200

    Add delegates from invoice to period

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,8 +4,16 @@ class Invoice < ApplicationRecord
   has_one :period
 
   delegate :name, to: :service, prefix: true
+  delegate :single, to: :period
+  delegate :period_start, to: :period
+  delegate :period_end, to: :period
 
   validates :service, presence: true
+  validates :period, presence: true
   validates :user, presence: true
   validates :price, presence: true, numericality: true
+
+  def strf_period
+    single ? I18n.t('invoices.single') : "#{I18n.l(period_start)}...#{I18n.l(period_end)}"
+  end
 end
