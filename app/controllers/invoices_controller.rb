f46d3a95ded3bfec46c4f3383a15f069cade3689
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2017-12-27 14:45:30 +0200

    Move graph matrix data method from controller to method, code cleanup

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,8 +1,8 @@
-class InvoicesController < ApplicationController
+# frozen_string_literal: true
 
-  before_action :set_invoice, only: [:show, :edit, :update, :destroy]
-  before_action :set_services, only: [:new, :edit]
-  before_action :get_index_data, only: [:index]
+class InvoicesController < ApplicationController
+  before_action :set_invoice, only: %i[show edit update destroy]
+  before_action :set_services, only: %i[new edit]
 
   def new; end
 
@@ -39,8 +39,20 @@ class InvoicesController < ApplicationController
   end
 
   def index
-    gon.data = @data
-    gon.options = @options
+    @invoices = current_user.invoices
+    # Yearly summary graph:
+    gon.data = Invoice.yearly_summary_graph_data(current_user)
+    options  = {
+      title: t('graphs.yearly_summary.title'),
+      hAxis: {
+        title: t('graphs.yearly_summary.x-title')
+      },
+      vAxis: {
+        title: t('graphs.yearly_summary.y-title')
+      },
+      download: true
+    }
+    gon.options = options
   end
 
   private
@@ -57,75 +69,5 @@ class InvoicesController < ApplicationController
 
   def set_services
     @services = Service.all
-  end
-
-  def get_index_data
-    @invoices = current_user.invoices
-
-    atm = Date.current
-    x_axis = ((atm - 1.year)..atm).map { |m| m.month }.uniq
-    graph_max_width = 12 # months
-    oldest_invoice_period = @invoices.first.period_start
-    if oldest_invoice_period > atm - 1.year
-      # If user has less than one year invoices saved to db:
-      graph_max_width = (atm.year * 12 + atm.month) - (oldest_invoice_period.year * 12 + oldest_invoice_period.month)
-      x_axis = x_axis[(12 - graph_max_width)..-1]
-    end
-
-    # Different services that user consumes:
-    services = Service.where(id: @invoices.pluck(:service_id).uniq)
-
-    @data = [['months'] + x_axis]
-    services.each do |s|
-      invoices = @invoices.where(service_id: s.id)
-      @data << [s.name] # add row for service-related invoice prices
-      invoices.each do |i|
-        if x_axis.include?(i.period_start.month)
-          @data.last << i.price.to_f
-        else
-          @data.last << 0
-        end
-      end
-      if @data.last.size != x_axis.size + 1
-        (x_axis.size - @data.last.size + 1).times do
-          @data.last << 0
-        end
-      end
-    end
-    @options  = {
-      title: t('graphs.yearly_summary.title'),
-      hAxis: {
-        title: t('graphs.yearly_summary.x-title'),
-      },
-      vAxis: {
-        title: t('graphs.yearly_summary.y-title'),
-      },
-      download: true
-    }
-    ####################################################################
-    months = Invoice.user_invoices_period_uniq_month_names(current_user)
-    ms_hash = {}
-    months.map { |m| ms_hash[m.to_sym] = nil }
-    arr = [['months'] + months]
-    services.each do |s|
-      invoices = @invoices.where(service_id: s.id)
-      periods = invoices.joins(:period).pluck(:period_start)#.map{ |p| I18n.t('date.month_names')[p.month-1] }
-      arr << [s.name]
-      ms_hash.each do |el|
-        i = invoices.joins(:period).find_by('period_start = ?', Date.new(2017, (I18n.t('date.month_names').index(el[0].to_s.downcase) + 1), 1) )
-        if i
-          arr.last << i.price.to_f
-        else
-          arr.last << 0
-        end
-      end
-    end
-    @data = arr.transpose#[
-    binding.pry
-      #['kuu', 'elekter', 'vesi'],
-      #['september', 47.8, 0],
-      #['oktoober', 49.1, 20.5],
-      #['november', 50.3, 0]
-    #] #@data.transpose
   end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Invoice < ApplicationRecord
   belongs_to :service
   belongs_to :user
   has_one :period
 
-  scope :current_year, lambda{ joins(:period).where(['period_start > ?', 1.year.ago]) }
+  scope :current_year, -> { joins(:period).where(['period_start > ?', 1.year.ago]) }
 
   delegate :name, to: :service, prefix: true
   delegate :single, to: :period
@@ -24,6 +26,27 @@ class Invoice < ApplicationRecord
   end
 
   def self.user_invoices_period_uniq_month_names(user)
-    user.invoices.current_year.pluck(:period_start).map {|p| I18n.t('date.abbr_month_names')[p.month]}.uniq
+    user.invoices.current_year.pluck(:period_start).map { |p| I18n.t('date.abbr_month_names')[p.month] }.uniq
+  end
+
+  def self.yearly_summary_graph_data(user)
+    user_invoices = user.invoices
+    # Different types of services that current user consumes:
+    services = Service.where(id: user_invoices.pluck(:service_id).uniq)
+    # last months that user has consumed some services (max 12 months):
+    invoice_months = Invoice.user_invoices_period_uniq_month_names(user)
+    # initialize graph data matrix with contents of x-axis ticks:
+    arr = [['months'] + invoice_months]
+    services.each do |s|
+      # all the invoices of certain service (max 12 months the oldest)
+      service_invoices = user_invoices.joins(:period).where(service_id: s.id)
+      arr << [s.name]
+      invoice_months.each do |month|
+        first_date_of_month = Date.new(2017, (I18n.t('date.abbr_month_names').index(month)), 1)
+        i = service_invoices.find_by('period_start = ?', first_date_of_month)
+        arr.last << (i ? i.price.to_f : 0)
+      end
+    end
+    arr.transpose # data-matrix for google charts ready
   end
 end
