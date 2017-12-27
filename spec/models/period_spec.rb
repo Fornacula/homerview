72cad3374e34f9c73e0aded498c5b79c3aaff075
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2017-12-27 19:30:14 +0200

    Annul period_end if invoice single

--- spec/models/period_spec.rb
+++ spec/models/period_spec.rb
@@ -4,4 +4,13 @@ require 'rails_helper'
 
 RSpec.describe Period, type: :model do
   it { is_expected.to belong_to(:invoice) }
+
+  it 'annulls period_end if single' do
+    invoice = create(:single_invoice)
+    period = invoice.period
+    period.period_end = Date.current
+    invoice.save
+    period.reload
+    expect(period.period_end).to be_nil
+  end
 end
