c3f82acbf78a676d617f9deb448d2ba1eae22f78
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2017-12-30 16:07:54 +0200

    Fix invoices form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def beginning_of_last_month
+    (Date.current - 1.month).beginning_of_month
+  end
+
+  def end_of_last_month
+    (Date.current - 1.month).end_of_month
+  end
 end
