402192a9a80c6540a09aa38514c30fa31a5c1c10
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2018-01-03 22:32:31 +0200

    Remove locale support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,10 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  before_action :authenticate_user!, :set_locale
+
+  before_action :authenticate_user!
 
   def new; end
   def show; end
   def edit; end
-
-  private
-
-  def set_locale
-    locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-      locale :
-      I18n.default_locale
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
-  end
 end
