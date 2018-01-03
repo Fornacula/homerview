964af93c9cd983280662ac4410d7e7b3fba034dd
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2018-01-03 22:37:38 +0200

    Add FormUser devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#welcome', as: 'welcome'
-  devise_for :users
+  devise_for :users, class_name: 'FormUser'
   resources :users, only: [:index]
   resources :services
   resources :invoices
