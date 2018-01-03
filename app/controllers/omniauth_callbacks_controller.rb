b73715dd66a85c781312ef25e49c2627cac1c2c9
Andres Ehrenpreis <andres.ehrenpreis@eenet.ee>
2018-01-03 23:44:48 +0200

    Fix omniauth callbacks controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,7 +10,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def generic_callback(provider)
-    @identity = Identity.find_for_oauth env['omniauth.auth']
+    @identity = Identity.find_for_oauth request.env['omniauth.auth']
 
     @user = @identity.user || current_user
     if @user.nil?
@@ -30,7 +30,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
     else
-      session["devise.#{provider}_data"] = env['omniauth.auth']
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
