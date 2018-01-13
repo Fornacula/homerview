b51a22bea4fb276bd2a2fa15f5ce6befbda9f14d
Andres Ehrenpreis <andres.ehrenpreis@gmail.com>
2018-01-14 01:13:00 +0200

    Add validations to invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -5,4 +5,11 @@ class Invitation < ApplicationRecord
   belongs_to :user
 
   validates :email, presence: true
+  # bug in shoulda-matchers, therefore _id needed:
+  validates :email, uniqueness: { scope: :community_id }
+  validates :email, format: { with: Devise.email_regexp }
+
+  def allowed_to_see?(user)
+    (community.master == user || self.user == user) ? true : false
+  end
 end
