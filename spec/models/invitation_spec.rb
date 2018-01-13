70267f325c1b98126077acb5811d782a75b45c23
Andres Ehrenpreis <andres.ehrenpreis@gmail.com>
2018-01-14 01:13:42 +0200

    Fix Inviting a member to community

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Invitation, type: :model do
   it { is_expected.to belong_to(:user) }
 
   it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).scoped_to(:community_id) }
+  it 'validates format of e-mail'
+
+  it 'shows to community master'
+  it 'shows to invitable'
+  it 'does not show to anybody else'
 end
