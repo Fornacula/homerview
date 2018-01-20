883e742a2003d56b96e7611482470452a6925db2
Andres Ehrenpreis <andres.ehrenpreis@gmail.com>
2018-01-20 12:36:11 +0200

    Fix users#index bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@
 
 class UsersController < ApplicationController
 
-  before_action :set_user, only: %i[show]
-
   def show
     @invitations = @user.invitations
   end
@@ -12,9 +10,4 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 end
